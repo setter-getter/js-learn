fe3da18cf9685016636339376d714739c151403a
Anna <88550551+ping335@users.noreply.github.com>
2022-01-20 05:35:09 +0300

    6-alert_prompt_confirm learn

---
+++ Basics/6-alert_prompt_confirm/learn/script.js
@@ -0,0 +1,13 @@
+// alert("Hello");
+
+// result = prompt(title, [default]);
+
+
+
+let age = prompt('Сколько тебе лет?', 100);
+
+alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+
+let isBoss = confirm("Ты здесь главный?");
+
+alert( isBoss ); // true, если нажата OK
