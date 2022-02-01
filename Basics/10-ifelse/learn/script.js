4a585396507ccb7aec92be874ff651e509420d3c
Anna <88550551+ping335@users.noreply.github.com>
2022-02-01 14:26:33 +0300

    add 10-ifelse

---
+++ Basics/10-ifelse/learn/script.js
@@ -0,0 +1,146 @@
+//if
+
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+
+// if (year == 2015) alert( 'Вы правы!' );
+
+//test
+
+// let myname = prompt("What is your name?");
+
+// if (myname == "Anya")
+//     {
+//         alert ("Hi, Admin!")
+//     };
+
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+
+// if (year == 2015) {
+//     alert( "Правильно!" );
+//     alert( "Ты такая умная!" );
+//   }
+
+// if (0) { // 0 is falsy
+//     alert("ola");
+// }
+
+// let year = prompt('2015?', '');
+// let condition = (year == 2015); // преобразуется к true или false
+
+// if (condition) {
+//   alert("test passed")
+// }
+
+//Блок «else»
+
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+
+// if (year == 2015) {
+//   alert( 'Да вы знаток!' );
+// } else {
+//   alert( 'А вот и неправильно!' ); // любое значение, кроме 2015
+// }
+
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+
+// if (year < 2015) {
+//   alert( 'Это слишком рано...' );
+// } else if (year > 2015) {
+//   alert( 'Это поздновато' );
+// } else {
+//   alert( 'Верно!' );
+// }
+
+// Условный оператор „?“
+
+// let accessAllowed;
+// let age = prompt('Сколько вам лет?', '');
+
+// if (age > 18) {
+//   accessAllowed = true;
+// } else {
+//   accessAllowed = false;
+// }
+
+// // alert(accessAllowed);
+
+// if(accessAllowed)
+// {
+//     alert("Проход разрешен");
+// } 
+// else
+// {
+//     alert("Проход закрыт");
+// }
+
+
+//? тернарный - т.к. имеет 3 аргумента
+
+// let result = условие ? значение1 : значение2;
+
+// let age = prompt("сколько вам лет?", "");
+
+// // let accessAllowed = (age > 18) ? true : false;
+
+// // то же самое
+// let accessAllowed = age > 18;
+
+// if (accessAllowed)
+// {
+//     alert("доступ разрешен");
+// }
+// else{
+//     alert("Возвращайся, когда позврослеешь:)");
+// }
+
+//Несколько операторов „?“
+
+// let age = prompt('Возраст?', 18);
+
+// let message = (age < 3) ? 'Здравствуй, малыш!' :
+//   (age < 18) ? 'Привет!' :
+//   (age < 100) ? 'Здравствуйте!' :
+//   'Какой необычный возраст!';
+
+// alert( message );
+
+// if (age < 3) {
+//     message = 'Здравствуй, малыш!';
+//   } else if (age < 18) {
+//     message = 'Привет!';
+//   } else if (age < 100) {
+//     message = 'Здравствуйте!';
+//   } else {
+//     message = 'Какой необычный возраст!';
+//   }
+
+//   alert( message );
+
+
+
+
+// оператор «вопросительный знак» ? используется в качестве замены if
+
+// let company = prompt('Какая компания создала JavaScript?', '');
+
+// (company == 'Netscape') ?
+//    alert('Верно!') : alert('Неправильно.');
+
+
+//    let company = prompt('Какая компания создала JavaScript?', '');
+
+//    if (company == 'Netscape') {
+//      alert('Верно!');
+//    } else {
+//      alert('Неправильно.');
+//    }
+
+
+
+
+
+
+
+
+
+
