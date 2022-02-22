f2202dbb75e2966b0eaa30b05409099227a99fc5
Anna <88550551+ping335@users.noreply.github.com>
2022-02-23 00:55:04 +0300

    add in function

--- Basics/15-function-basics/script2.js
+++ Basics/15-function-basics/script2.js
@@ -1,16 +1,27 @@
 // Возможно использовать return и без значения. 
 // Это приведёт к немедленному выходу из функции.
 
-function showMovie(age) {
-    if ( !checkAge(age) ) {
-      return;
-    }
+// function showMovie(age) {
+//     if ( !checkAge(age) ) {
+//       return;
+//     }
   
-    alert( "Вам показывается кино" ); // (*)
-    // ...
+//     alert( "Вам показывается кино" ); // (*)
+//     // ...
+//   }
+
+// Функции == Комментарии
+
+function showPrimes(n) {
+    nextPrime: for (let i = 2; i < n; i++) {
+  
+      for (let j = 2; j < i; j++) {
+        if (i % j == 0) continue nextPrime;
+      }
+  
+      alert( i ); // простое
+    }
   }
-
-
 
 
 
