66c77cda3772bade08b17372cbc14221ae6a7367
Anna <10129210-ping335@users.noreply.gitlab.com>
2022-03-06 21:54:40 +0300

    add files

--- Basics/17-arrow-functions-basics/script.js
+++ Basics/17-arrow-functions-basics/script.js
@@ -1,3 +1,25 @@
 // Функции-стрелки, основы
 
 //~ let func = (arg1, arg2, ...argN) => expression;
+//~ это более короткий вариант такой записи:
+
+//~ let func = function(arg1, arg2, ...argN) {
+//~   return expression;
+//~ };
+
+
+let sum = (a, b) => a + b;
+
+/* Более короткая форма для:
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+
+alert( sum(1, 2) ); // 3
+
+
+
+
+
